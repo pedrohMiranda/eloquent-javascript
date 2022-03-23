76b100b5586c97b482bd892855b4e69a22517b9e
pedroMiranda <pedrohmdmiranda@gmail.com>
2022-03-22 23:51:50 -0300

    Deep comparison solution

---
+++ 4 - Data Structures - Objects and Arrays/Deep comparison/solution.js
@@ -0,0 +1,33 @@
+function deepEqual(value1, value2) {
+    if (typeof(value1) !== 'object' && typeof(value2) !== 'object' && value1, value2 == null){
+        return (value1 === value2)
+    }
+
+    let equal = true;
+
+    function objEquality(value1, value2) {
+        Object.keys(value1).forEach(key => {
+            if (equal === false) return;
+            
+            if (typeof(value1[key]) === 'object' && value1[key] !== null){
+                objEquality(value1[key], value2[key])
+            }
+            else if (value1[key] != value2[key]){
+                equal = false;
+                return;
+            }
+        });
+    }
+
+    objEquality (value1, value2);
+
+    return equal;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
